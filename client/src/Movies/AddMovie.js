86064721b52c0d814061e791d4b0602b6fa018e3
Ruth Poliakon <r.poliakon@gmail.com>
2019-11-07 18:48:17 -0700

    add movie star functionality

--- client/src/Movies/AddMovie.js
+++ client/src/Movies/AddMovie.js
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import axios from "axios";
 
 function AddMovie(props) {
+  const [actors, setActors] = useState({
+    actor1: "",
+    actor2: "",
+    actor3: ""
+  });
+
   const [newMovie, setNewMovie] = useState({
     id: "",
     title: "",
@@ -17,8 +23,20 @@ function AddMovie(props) {
     });
   };
 
+  const handleActorChange = event => {
+    setActors({
+      ...actors,
+      [event.target.name]: event.target.value
+    });
+  };
+
   const handleSubmit = event => {
     event.preventDefault();
+
+    newMovie.stars.push(actors.actor1);
+    newMovie.stars.push(actors.actor2);
+    newMovie.stars.push(actors.actor3);
+
     console.log(newMovie);
     axios
       .post("http://localhost:5000/api/movies", newMovie)
@@ -64,10 +82,26 @@ function AddMovie(props) {
           <input
             className="form-star"
             type="text"
-            name="star"
-            placeholder="actor1,actor2,actor3"
-            value={newMovie.stars}
-            onChange={handleChange}
+            name="actor1"
+            placeholder="actor 1"
+            value={actors.actor1}
+            onChange={handleActorChange}
+          />
+          <input
+            className="form-star"
+            type="text"
+            name="actor2"
+            placeholder="actor 2"
+            value={actors.actor2}
+            onChange={handleActorChange}
+          />
+          <input
+            className="form-star"
+            type="text"
+            name="actor3"
+            placeholder="actor 3"
+            value={actors.actor3}
+            onChange={handleActorChange}
           />
         </div>
         <button className="update-button" type="submit">
