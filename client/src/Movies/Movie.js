c0115020068ef416894e2e4fc9b1dba7165a4744
Ruth Poliakon <r.poliakon@gmail.com>
2019-11-07 17:15:42 -0700

    delete functionality

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -32,6 +32,22 @@ export default class Movie extends React.Component {
     addToSavedList(this.state.movie);
   };
 
+  deleteMovie = (event, id) => {
+    event.preventDefault();
+
+    if (window.confirm("Are you sure you want to delete this movie?")) {
+      axios
+        .delete(`http://localhost:5000/api/movies/${id}`)
+        .then(result => {
+          console.log("Movie was deleted");
+          this.props.history.push("/");
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  };
+
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
@@ -49,6 +65,12 @@ export default class Movie extends React.Component {
         >
           Edit
         </Link>
+        <div
+          className="delete-button"
+          onClick={e => this.deleteMovie(e, this.state.movie.id)}
+        >
+          Delete
+        </div>
       </div>
     );
   }
